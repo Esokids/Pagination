d62bc9dccee16fe8ab4cb48e6093a50fba0f6322
Esokids <mawin_07@hotmail.com>
2020-08-13 20:52:26 +0700

    Pagination .NET MVC Core

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pagination.Data;
+using Pagination.Models;
+
+namespace Pagination.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDb db;
+
+        public void SetPerson()
+        {
+            for (int i = 1; i <= 35; i++)
+            {
+                Person person = new Person { Name = $"Person{i}" };
+                db.Add(person);
+                db.SaveChanges();
+            }
+        }
+
+        public HomeController(ILogger<HomeController> logger, AppDb db)
+        {
+            _logger = logger;
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            //SetPerson();
+            return View();
+        }
+
+        public IActionResult Person()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Data/AppDb.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pagination.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pagination.Data
+{
+    public class AppDb : DbContext
+    {
+        public AppDb(DbContextOptions<AppDb> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
